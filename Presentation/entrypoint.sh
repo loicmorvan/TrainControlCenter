5eb0d000a701ea8c74fd4f1d18e39d8e4b805df0
Loïc Morvan <loic.morvan.fr@gmail.com>
2021-03-04 22:36:46 +0100

    Configured docker compose

---
+++ Presentation/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+echo "{" > /usr/local/webapp/nginx/html/appsettings.json
+echo "  network-edition-url: ${NETWORK_EDITION_URL}" >> /usr/local/webapp/nginx/html/appsettings.json
+echo "}" >> /usr/local/webapp/nginx/html/appsettings.json
+
+cat /usr/local/webapp/nginx/html/appsettings.json
+
+nginx -g "daemon off;"
